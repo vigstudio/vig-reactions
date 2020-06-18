c6e24b61d8ebd7c63d20fa08612bcd0a3c76f7fd
Pham Viet Nghia <vingamagic@gmail.com>
2020-06-18 13:38:36 +0700

    1.0.9

--- src/Http/Controllers/ActionController.php
+++ src/Http/Controllers/ActionController.php
@@ -4,12 +4,12 @@ namespace Botble\VigReactions\Http\Controllers;
 
 use Botble\VigReactions\Repositories\Interfaces\VigReactionsInterface;
 use Botble\Base\Http\Controllers\BaseController;
-use Illuminate\Http\Request;
-use Exception;
 use Botble\Base\Http\Responses\BaseHttpResponse;
 use Botble\VigReactions\Http\Requests\VigReactionsRequest;
 use Botble\VigReactions\Models\VigReactions;
+use Illuminate\Http\Request;
 use Illuminate\Session\Store;
+use Exception;
 use Session;
 use Eloquent;
 
@@ -37,38 +37,49 @@ class ActionController extends BaseController
             return response()->json(['error' => true]);
         }
 
-        if(auth()->guard('member')->check()) {
-            $request->merge([
-                'session_id'    => $session_id,
-                'user_id'       => auth()->guard('member')->id(),
-                'user_type'     => get_class(auth()->guard('member')->user())
-            ]);
-        }
+        $request->merge($this->mergeRequest($session_id));
 
-        if(auth()->check()) {
-            $request->merge([
-                'session_id'    => $session_id,
-                'user_id'       => auth()->id(),
-                'user_type'     => get_class(auth()->user())
-            ]);
-        }
+        $query = $this->vigReactionsRepository->select(['*'], [
+            'session_id'    => $session_id,
+            'reaction_id'   => $request->input('reaction_id'),
+            'reaction_type' => $request->input('reaction_type')
+        ]);
 
-        $query = VigReactions::where('session_id', $session_id)
-                                ->where('reaction_id', $request->input('reaction_id'))
-                                ->where('reaction_type', $request->input('reaction_type'))
-                                ->get();
+        $reaction = $this->createOrUpdate($query, $request);
 
+        return response()->json($reaction);
+    }
+
+    public function createOrUpdate($query, $request)
+    {
         if($query->count() == 0) {
-            $reaction = $this->vigReactionsRepository->create($request->input());
+            $result = $this->vigReactionsRepository->create($request->input());
         } else if($query->first()->type !== $request->input('type')) {
             $old_type = $query->first()->type;
             $query->first()->update($request->input());
-            $reaction = ['old_type' => $old_type, 'action' => 'update', 'type' => $query->first()->type];
+            $result = ['old_type' => $old_type, 'action' => 'update', 'type' => $query->first()->type];
         } else {
-            $reaction = ['type' => $query->first()->type, 'action' => 'delete'];
+            $result = ['type' => $query->first()->type, 'action' => 'delete'];
             $query->first()->delete();
         }
-        return response()->json($reaction);
+        return $result;
+    }
+
+    public function mergeRequest($session)
+    {
+        if(auth()->guard('member')->check()) {
+            $user_id = auth()->guard('member')->id();
+            $user_type = get_class(auth()->guard('member')->user());
+        } else if(auth()->check()) {
+            $user_id = auth()->id();
+            $user_type = get_class(auth()->user());
+        }
+
+        return [
+            'session_id'    => $session,
+            'user_id'       => $user_id,
+            'user_type'     => $user_type
+        ];
     }
 
 
